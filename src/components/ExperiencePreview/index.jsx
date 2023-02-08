3bd1657d8370543fa5cd1dde56b82d1dd6808cdb
giorgi <giorgikochuashvili2002@gmail.com>
2023-02-08 22:19:28 +0400

    added ExperiencePreview component

--- src/components/ExperiencePreview/index.jsx
+++ src/components/ExperiencePreview/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
-
-const ExperiencePreview = () => {
-  return <div>ExperiencePreview</div>;
+import './styles.css';
+const ExperiencePreview = ({ field }) => {
+  return (
+    <div className='result-experience-container'>
+      <div className='result-job-container'>
+        <div className='result-experience-position'>
+          <p>{field.position}</p>
+        </div>
+        <div className='result-employer-position'>{field.employer}</div>
+      </div>
+      <div className='result-duration-container'>
+        <div className='result-experience-startDate'>
+          <p>{field.start_date}</p>
+        </div>
+        {field.start_date && field.due_date && <p className='dash'>-</p>}
+        <div className='result-experience-endDate'>
+          <p>{field.due_date}</p>
+        </div>
+      </div>
+      <div className='result-description-container'>
+        <div className='result-experience-description'>
+          <p>{field.description}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExperiencePreview;
--- src/components/Preview/index.jsx
+++ src/components/Preview/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles.css';
 import PrivateInfoPreview from '../PrivateInfoPreview';
+import ExperiencePreview from '../ExperiencePreview';
 import watermark from '../../images/watermark.svg';
 import { useContext } from 'react';
 import { UserContext } from '../../context/global.context';
@@ -16,40 +17,17 @@ const Preview = () => {
     >
       <img src={watermark} alt='' className='watermark' />
       <PrivateInfoPreview inputsInfo={inputsInfo} />
-      {page > 1 ? <div className='result-underline'></div> : ''}
-      {page > 1 ? <h2 className='result-title with-desc'>გამოცდილება</h2> : ''}
-      {page > 1
-        ? inputsInfo.experiences.map((field, index) => {
-            return (
-              <div className='result-experience-container' key={index}>
-                <div className='result-job-container'>
-                  <div className='result-experience-position'>
-                    <p>{field.position}</p>
-                  </div>
-                  <div className='result-employer-position'>
-                    {field.employer}
-                  </div>
-                </div>
-                <div className='result-duration-container'>
-                  <div className='result-experience-startDate'>
-                    <p>{field.start_date}</p>
-                  </div>
-                  {field.start_date && field.due_date && (
-                    <p className='dash'>-</p>
-                  )}
-                  <div className='result-experience-endDate'>
-                    <p>{field.due_date}</p>
-                  </div>
-                </div>
-                <div className='result-description-container'>
-                  <div className='result-experience-description'>
-                    <p>{field.description}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })
-        : ''}
+      {page > 1 ? (
+        <>
+          <div className='result-underline'></div>
+          <h2 className='result-title with-desc'>გამოცდილება</h2>
+          {inputsInfo.experiences.map((field, index) => {
+            return <ExperiencePreview field={field} key={index} />;
+          })}
+        </>
+      ) : (
+        ''
+      )}
 
       {page > 2 ? <div className='result-underline'></div> : ''}
       {page > 2 ? <h2 className='result-title with-desc'>განათლება</h2> : ''}
